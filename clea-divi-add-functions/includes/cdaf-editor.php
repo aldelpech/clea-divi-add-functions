fad7e3c808eb3eadd991e052a51e7104ffba38f4
Anne-Laure Delpech <contact@parcours-performance.com>
2017-10-08 12:09:11 +0200

    text colors in editor is the divi default color palette !

--- clea-divi-add-functions/includes/cdaf-editor.php
+++ clea-divi-add-functions/includes/cdaf-editor.php
@@ -1,16 +1,13 @@
 <?php
 
 
-/* add custom colors to the editor */
-// add_filter('tiny_mce_before_init', 'cdaf_custom_colors_mce4_options');
 
-function cdaf_custom_colors_mce4_options($init) {
-
+/*	
 	$cdaf_colors = cdaf_read_color_palette() ; 
 	$cdaf_nb_color = count( $cdaf_colors ) ;
 	$index = 1 ;
 	$custom_colours =  "'";
-/*	
+
 	foreach ( $cdaf_colors as $color ) {
 		
 		// remove # in color code
@@ -42,47 +39,13 @@ function cdaf_custom_colors_mce4_options($init) {
 	
 	$custom_colours .=  "'";
 
-	
-*/ 
-// Le format doit être '"color HEX", "color-name", \r\n
-$custom_colours = '
-        "3366FF", "Color 1 name",
-        "CCFFCC", "Color 2 name",
-        "FFFF00", "Color 3 name",
-        "99CC00", "Color 4 name",
-        "FF0000", "Color 5 name",
-        "FF99CC", "Color 6 name",
-        "CCFFFF", "Color 7 name"
-    ';	
-
-$custom_colours = '
-  "e8541e", "Orange1", "f39c2c", "Orange2", "c0f000", "Vert", "ffffff", "Blanc", "000000", "Noir"
-  ';
- $default_colors = '
-    "000000", "Black",        "993300", "Burnt orange", "333300", "Dark olive",   "003300", "Dark green",   "003366", "Dark azure",   "000080", "Navy Blue",      "333399", "Indigo",       "333333", "Very dark gray", 
-    "800000", "Maroon",       "FF6600", "Orange",       "808000", "Olive",        "008000", "Green",        "008080", "Teal",         "0000FF", "Blue",           "666699", "Grayish blue", "808080", "Gray", 
-    "FF0000", "Red",          "FF9900", "Amber",        "99CC00", "Yellow green", "339966", "Sea green",    "33CCCC", "Turquoise",    "3366FF", "Royal blue",     "800080", "Purple",       "999999", "Medium gray", 
-    "FF00FF", "Magenta",      "FFCC00", "Gold",         "FFFF00", "Yellow",       "00FF00", "Lime",         "00FFFF", "Aqua",         "00CCFF", "Sky blue",       "993366", "Brown",        "C0C0C0", "Silver", 
-    "FF99CC", "Pink",         "FFCC99", "Peach",        "FFFF99", "Light yellow", "CCFFCC", "Pale green",   "CCFFFF", "Pale cyan",    "99CCFF", "Light sky blue", "CC99FF", "Plum",         "FFFFFF", "White"
-    ';
-	// build colour grid custom colors
-	// see https://wordpress.stackexchange.com/questions/233450/how-do-you-add-custom-color-swatches-to-all-wysiwyg-editors
-	$init['textcolor_map'] = '['.$custom_colours.']';
-	$init['textcolor_map'] = '['.$custom_colors.','.$default_colors.']';
-	
-	// debug 
-	// echo "<p>" . $custom_colours . "</p>" ;
-	// enable 6th row for custom colours in grid
-	$init['textcolor_rows'] = 6;
-
-	return $init;
-}	
+*/	
 
 
 function cdaf_editor_colors($init) {
 
 	// https://wordpress.stackexchange.com/questions/233450/how-do-you-add-custom-color-swatches-to-all-wysiwyg-editors
-    $custom_colours = '
+    /* $custom_colours = '
         "3366FF", "Color 1 name",
         "CCFFCC", "Color 2 name",
         "FFFF00", "Color 3 name",
@@ -91,6 +54,32 @@ function cdaf_editor_colors($init) {
         "FF99CC", "Color 6 name",
         "CCFFFF", "Color 7 name"
     ';
+*/
+
+
+	$cdaf_colors = cdaf_read_color_palette() ; 
+	$cdaf_nb_color = count( $cdaf_colors ) ;
+	$index = 0 ;
+	$default_colors =  array();
+
+	foreach ( $cdaf_colors as $color ) {
+		
+		// remove # in color code
+		$color = ltrim( $color, "#" ) ;
+		// met en majuscule le code HEX
+		$color = strtoupper( $color );
+		$default_colors[] = array( $color, 'color ' . $index  )  ;
+		
+		$index++ ; 						
+	}
+	
+$custom_colours = wp_json_encode( $default_colors ) ;
+$replace = array( "[", "]") ; // we don't want these in the final string
+$custom_colours = str_replace($replace, "", $custom_colours );
+// $custom_colours = "'" . $custom_colours . "'" ;
+
+
+$custom_colours_a = '"423432","color 0","FFFFFF","color 1","4C858D","color 2","ED693B","color 3","F28A2B","color 4","FAC079","color 5","F6DB6B","color 6","A60F65","color 7"' ;
 
     // build colour grid default+custom colors
     $init['textcolor_map'] = '['.$custom_colours.']';
@@ -99,6 +88,9 @@ function cdaf_editor_colors($init) {
     // 8 swatches per row
     $init['textcolor_rows'] = 1;
 
+	// debug 
+	echo "<p>" . $custom_colours . "</p>" ;
+	echo "<p>A " . $custom_colours_a . "</p>" ;
     return $init;
 }
 add_filter('tiny_mce_before_init', 'cdaf_editor_colors');
