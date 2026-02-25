a720aed921f33db7cf4be15fd19a9a997baaa009
Anne-Laure Delpech <aldelpech@users.noreply.github.com>
2026-02-25 12:25:25 +0100

    Update cdaf-utilities.php
    MAJ de sécurité

--- clea-divi-add-functions/includes/cdaf-utilities.php
+++ clea-divi-add-functions/includes/cdaf-utilities.php
@@ -7,10 +7,17 @@
 *
 ************************************************************************/
 
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Récupère la palette de couleurs Divi sous forme de tableau
+ */
 function cdaf_read_color_palette() {
-	
-	$cdaf_color_palette = et_get_option( 'divi_color_palette', false ) ; 
-	$return = explode("|", $cdaf_color_palette);
-	return $return ;
-	
+    $palette_string = et_get_option( 'divi_color_palette', '' );
+    if ( empty( $palette_string ) ) {
+        return [];
+    }
+    // Transforme la chaîne "color1|color2|..." en tableau
+    return explode( '|', $palette_string );
 }
